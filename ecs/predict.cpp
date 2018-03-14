874663ca1abe9ab7c5bb9c9167d8ea0ea7ba28c2
zonxin <0zonxin0@163.com>
2018-03-14 23:44:57 +0800

    修改predect.cpp, 为 result_file 分配空间

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -1,7 +1,9 @@
 #include "predict.h"
 #include <stdio.h>
+#include <string.h>
 #include "input.h"
 #include "flavorCollect.h"
+#include "PMAlloc.h"
 
 //你要完成的功能总入口
 void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int data_num, char * filename)
@@ -20,6 +22,8 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
     readTrainData(data, data_num, tdlist, vmlist);
 
     PHYmachine pm = PM_getPM();
+	char * result_file = (char *)malloc(data_num*50);
+    *result_file='\0'; char *outs = result_file;
     /**
      *@var PHYmachine   pm           物理机信息
      *@var FLavorList   vmlist       需要预测的虚拟机列表
@@ -27,7 +31,7 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
      *@var time_t startTime, endTime 要预测的时间开始和结束
      *@var TDList        tdlist      训练数据 
      */
-    
+    PMList pml = newPMList(data_num);
     // to do
     
     FlavorCollectList fcs = collectFlavorByDay(vmlist,tdlist);
@@ -68,10 +72,11 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
 
 
 	// 需要输出的内容
-	char * result_file = (char *)"17\n\n0 8 0 20";
+    PMList_sprintf(pml,outs);
 
 	// 直接调用输出文件的方法输出到指定文件中(ps请注意格式的正确性，如果有解，第一行只有一个数据；第二行为空；第三行开始才是具体的数据，数据之间用一个空格分隔开)
 	write_result(result_file, filename);
+    free(result_file);
     free(fcs);
     free(vmlist);
     free(tdlist);
