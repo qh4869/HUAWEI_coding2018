496d5dd7be931757787094df5625bb95a2a3968e
qh4869 <WDqiuhao@163.com>
2018-03-14 18:51:56 +0800

    180314release

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "input.h"
 #include "flavorCollect.h"
+#include "string.h"
 
 //你要完成的功能总入口
 void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int data_num, char * filename)
@@ -19,7 +20,8 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
     TDList tdlist = newTDList(MAX_DATA_NUM);
     readTrainData(data, data_num, tdlist, vmlist);
 
-    PHYmachine pm = PM_getPM();
+    //PHYmachine pm = PM_getPM();
+    
     /**
      *@var PHYmachine   pm           物理机信息
      *@var FLavorList   vmlist       需要预测的虚拟机列表
@@ -32,39 +34,68 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
     
     FlavorCollectList fcs = collectFlavorByDay(vmlist,tdlist);
 
-
-    printf("========PHY Info===================\n");
-    printf("PHY: %d %dMB\n",pm->cpuNumber,pm->memSize);
-    printf("========Flavor Info===================\n");
     int i;
     Flavor fl;
-    FlavorList_foreach(fl,vmlist,i) {
-        printf("%s %d %d\n", fl->flavorType, fl->cpuNumber, fl->memSize);
-    }
     double diff = difftime(endTime,startTime);
-    printf("startTime:%ld endTime:%ld diff:%f\n",
-            startTime, endTime, diff/3600/24);
-    printf("========training data===============\n");
-    printf("flavor type\tcreateDay\tcreateTime\n");
-    TDItem item; 
-    TDList_foreach(item,tdlist,i) {
-        printf("%s\t%d\t%ld\n",
-                item->flavor->flavorType,
-                item->createDay,
-                item->createTime);
-    }
-
-    printf("========training data Collects===============\n");
+    int numOfDaysToPredict = (int)diff/3600/24;
+    char flavorIDTmp[100],flavorTypeTmp[20],dateTmp[100],timeTmp[30]; 
+    sscanf(data[0],"%s\t%s\t%s %s\n",flavorIDTmp,flavorTypeTmp,dateTmp,timeTmp);
+    strcat(dateTmp," ");
+    strcat(dateTmp, timeTmp);
+    int trainStartDay = readTime(dateTmp) / (3600*24);
+    sscanf(data[data_num-1],"%s\t%s\t%s %s\n",flavorIDTmp,flavorTypeTmp,dateTmp,timeTmp);
+    strcat(dateTmp," ");
+    strcat(dateTmp, timeTmp);
+    int trainEndDay = readTime(dateTmp) / (3600*24);
+    int numOfDaysToTrain = trainEndDay - trainStartDay;
     FlavorList_foreach(fl,vmlist,i) {
         int idx = fl->idx;
-        printf("Flavor Type:%s\n",fcs->collects[idx].flavor->flavorType);
         CollectNode pos; 
         struct list_head *ptr,*head = &(fcs->collects[idx].head);
+        int tmpSum = 0;
         for(ptr = head->next; ptr !=head; ptr = ptr->next) {
             pos = container_of(ptr,collect_node_t, nodeptr);
-            printf("%d,%d\n",pos->key,pos->count);
+            tmpSum += pos->count;
+            
         }
+        double predictNum = (double)tmpSum / numOfDaysToTrain * numOfDaysToPredict;       
+        printf("idx:%d\tsum:%d\n", idx, (int)(predictNum+0.5));
     }
+
+    //========================================================
+
+  //  printf("========PHY Info===================\n");
+  //  printf("PHY: %d %dMB\n",pm->cpuNumber,pm->memSize);
+  //  printf("========Flavor Info===================\n");
+  //  int i;
+  //  Flavor fl;
+  //  FlavorList_foreach(fl,vmlist,i) {
+  //      printf("%s %d %d\n", fl->flavorType, fl->cpuNumber, fl->memSize);
+  //  }
+  //  double diff = difftime(endTime,startTime);
+  //  printf("startTime:%ld endTime:%ld diff:%f\n",
+  //          startTime, endTime, diff/3600/24);
+  //  printf("========training data===============\n");
+  //  printf("flavor type\tcreateDay\tcreateTime\n");
+  //  TDItem item; 
+  //  TDList_foreach(item,tdlist,i) {
+  //      printf("%s\t%d\t%ld\n",
+  //              item->flavor->flavorType,
+  //              item->createDay,
+  //              item->createTime);
+  //  }
+
+  //  printf("========training data Collects===============\n");
+  //  FlavorList_foreach(fl,vmlist,i) {
+  //      int idx = fl->idx;
+  //      printf("Flavor Type:%s\n",fcs->collects[idx].flavor->flavorType);
+  //      CollectNode pos; 
+  //      struct list_head *ptr,*head = &(fcs->collects[idx].head);
+  //      for(ptr = head->next; ptr !=head; ptr = ptr->next) {
+  //          pos = container_of(ptr,collect_node_t, nodeptr);
+  //          printf("%d,%d\n",pos->key,pos->count);
+  //      }
+  //  }
 
 
 	// 需要输出的内容
