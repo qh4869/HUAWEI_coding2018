d8c91008125becf7b70e532b0978ec9aff44a5c0
zonxin <0zonxin0@163.com>
2018-03-15 11:35:15 +0800

    Type FlavorIntST,未测试

---
+++ ecs/FlavorIntST.cpp
@@ -0,0 +1,55 @@
+#include "FlavorIntST.h"
+
+FlavorIntST newFlavorIntST() {
+    FlavorIntST st = malloc(sizeof(struct _flavor_int_st_t));
+    memset(st,0,sizeof(struct _flavor_int_st_t));
+    st -> _capacity = MAX_FLAVOR_TYPE;
+    return st;
+}
+
+int FlavorIntST_put(FlavorIntST st,Flavor key, int value)
+{
+    int i;
+    for(i=0; i<st->_size; i++) {
+        if(st->_map[i].key == key) { break; }
+    }
+    if(i>=st->_size) { st->_size++; }
+    st->_map[key].key = key;
+    st->_map[key].value = value;
+    return 1;
+}
+
+/* 返回指向值的指针  if exsit 
+ * 返回 NULL         else */
+int *FlavorIntST_get(FlavorList st, Flavor key)
+{
+    int i;
+    for(i=0; i<st->_size; i++) {
+        if(st->_map[i].key == key) { 
+            return &(st->_map[i].value);
+        }
+    }
+    return NULL;
+} 
+
+/***************************************************/
+int FlavorIntSt_sumAll(FlavorIntST st)
+{
+    int i,sum=0;
+    for(i=0; i<st->_size; i++) {
+        sum += st->_map[i].value;
+    }
+    return sum;
+}
+int FlavorIntSt_sprintf(FlavorIntST st, char *outs)
+{
+    char *oldouts = outs;
+    int chars, i;
+    for(i=0; i<st->_size; i++) {
+        chars = sprintf(outs,"%s %d\n",
+                st->_map[i].key->flavorType,
+                st->_map[i].value);
+        outs += chars;
+    }
+    return outs - oldouts;
+}
---
+++ ecs/FlavorIntST.h
@@ -0,0 +1,21 @@
+#ifndef __FLAVORINTST_H__
+#define __FLAVORINTST_H__
+#include "predict.h"
+
+typedef struct _flavor_int_st_t{
+    struct {
+        Flavor key;
+        int value;
+    } _map[MAX_FLAVOR_TYPE];
+    int _size;
+    int _capacity;
+}*FlavorIntST;
+
+FlavorIntST newFlavorIntST();
+int FlavorIntST_put(FlavorIntST st,Flavor key, int value);
+int *FlavorIntST_get(FlavorList st, Flavor key);
+/***************************************************/
+int FlavorIntSt_sumAll(FlavorIntST st);
+int FlavorIntSt_sprintf(FlavorIntST st, char *outs);
+#endif
+
